d91eaebeb38108c811ee24234c25eda169d6ea6b
Rokas <rookas.rudzenskas@gmail.com>
2023-09-19 12:58:20 +0300

    🔧 chore(aws.xml): update activeProfile and recentlyUsedProfiles to use profile:byrookas 🔧 chore(layout.tsx): update title and description in metadata to reflect JS Mastery branding 🔧 chore(layout.tsx): update og:url in metadata to use jsmastery.pro domain 🔧 chore(Footer.tsx): update copyright text to reflect ByRookas branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,14 @@ import './globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Storage App',
-  description: 'Storage App Resources',
+  title: 'JS Mastery',
+  description: 'JS Mastery Resources',
   other: {
     'theme-color': '#0d1117',
     "color-scheme": "dark only",
     "twitter:image": 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
     "twitter:card": "summary_large_image",
-    "og:url": "byrookas.com",
+    "og:url": "jsmastery.pro",
     "og:image": 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
     "og:type": "website",
   }
